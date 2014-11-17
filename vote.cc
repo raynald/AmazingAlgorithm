458a5e2a9462aaf621d0ca700c4110ee43bc5b4d
raynald <raynaldchung@gmail.com>
2014-11-17 10:19:32 +0100

    vote algorithm

--- vote.cc
+++ vote.cc
@@ -1,44 +1,69 @@
 #include<iostream>
 #include<map>
-/* Moves all elements that appear more than n/k times to the front of the range.
- *  * Returns the end of this range of elements.
- *   * O(n log k) time using O(k) memory. */
-template<class FIterator>
-FIterator find_repeats(FIterator first, FIterator last, int k) {
+
+using namespace std;
+
+int A[110];
+int N, k;
+map<int, int> mmap;
+map<int, int>::iterator got;
+
+struct cmp {
+    int val;
+    int count;
+    cmp() {
+        val = count = 0;
+    }
+    cmp(int x, int y): val(x), count(y) {}
+};
+
+//Like Tetris
+int find() {
     /* Compute the candidate elements in one pass. */
-    int N = 0;
-    std::map<typeof(*first), int> count;
-    for(FIterator it = first; it != last; ++it, ++N) {
-        ++count[*it];
-        if(count.size() == k) {
-            for(typeof(count.begin()) jt = count.begin(), tmp; jt != count.end(); ) {
-                tmp = jt++;
-                if(--tmp->second == 0) {
-                    count.erase(tmp);
+    cmp mat[k];
+
+    for(int i=0;i<N;i++) {
+        got = mmap.find(A[i]);
+        if(got!=mmap.end()) {
+            got->second++;
+        }
+        else {
+            mmap.insert(pair<int, int>(A[i],1));
+        }
+        if(mmap.size()==k) {
+            for(got = mmap.begin();got!=mmap.end();got++) {
+                if(got->second==1) {
+                    mmap.erase(got->first);
                 }
+                else got->second--;
             }
         }
     }
-    std::cout << count.size() << std::endl;
-    /* Verify the candidate elements in a second pass. */
-    FIterator result = first;
-    std::map<typeof(*first), int> candidate_count;
-    for(FIterator it = first; it != last; ++it) {
-        if(count.find(*it) != count.end() && ++candidate_count[*it] == N / k + 1) {
-            std::swap(*result++, *it);
+    for(got = mmap.begin();got!=mmap.end();got++) {
+        got->second=0;
+    }
+    for(int i=0;i<N;i++) {
+        got = mmap.find(A[i]);
+        if(got!=mmap.end()) {
+            got->second++;
         }
     }
-    std::sort(first, result);
-    return result;
+    for(got = mmap.begin();got!=mmap.end();got++) {
+        if(got->second<N/k+1) {
+            mmap.erase(got->first);
+        }
+    }
 }
 
 int main() {
-    int N = 10;
-    //int A[] = {7, 6, 6, 8, 5, 3, 6, 2, 8, 1};
-    int A[] = {7, 6, 6, 8, 6, 6, 6, 5, 8};
-    int M = find_repeats(A, A + N, 3) - A;
-    for(int i = 0; i < M; i++) {
-        std::cout << A[i] << std::endl;
+
+    cin >> N >> k;
+    for(int i=0;i<N;i++) {
+        cin >> A[i];
+    }
+    int M = find();
+    for(got = mmap.begin();got!=mmap.end();got++) {
+        cout << got->first <<endl;
     }
     return 0;
 }
