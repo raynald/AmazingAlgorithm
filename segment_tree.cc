4cbe367096c4c0f5ba23a736498d518acd842991
raynald <raynaldchung@gmail.com>
2014-12-23 16:18:06 +0100

    shell sort

--- segment_tree.cc
+++ segment_tree.cc
@@ -1,29 +1,54 @@
+#include<iostream>
+#include<algorithm>
+#include<cstdio>
+#include<climits>
 #define Maxn 10000
+
+using namespace std;
+
+int val[1010];
+int maxx, minx;
 
 class Segment_Tree {
 private:
     struct Node {
-        int a,b,left,right,cover;
+        int left,right,cover;
+        int max, min;
     };
     Node Tree[Maxn];
-    int Number,Tot, c, d;
-public
-    void build(int a,int b) {
-        int Now;
-        Tot++;
-        Now=Tot;
-        Tree[Now].a=a;
-        Tree[Now].b=b;
-        Tree[Now].cover=0;
+    int Number, c, d;
+public:
+    Segment_Tree() {
+    }
+
+    void build(int Now, int a,int b) {
+        Tree[Now].left=a;
+        Tree[Now].right=b;
+        //Tree[Now].cover=0;
         if(b-a>=1) {
-            intmid=(a+b)>>1;
-            Tree[Now].left=Tot+1;
-            build(a,mid);
-            Tree[Now].right=Tot+1;
-            build(mid+1,b);
+            int mid=(a+b)>>1;
+            Tree[Now].left=a;
+            build(2*Now+1 , a,mid);
+            Tree[Now].right=b;
+            build(2*Now+2,mid+1, b);
+            Tree[Now].max = max(Tree[2*Now+1].max, Tree[2*Now+2].max);
+            Tree[Now].min = min(Tree[2*Now+1].min, Tree[2*Now+2].min);
+        }
+        else {
+            Tree[Now].left = Tree[Now].right = a;
+            Tree[Now].max = val[a];
+            Tree[Now].min = val[a];
         }
     }
 
+    void print(int Now) {
+        cout << Tree[Now].left << " " << Tree[Now].right <<  " " << Tree[Now].max << " " << Tree[Now].min << endl;
+        if(Tree[Now].right>Tree[Now].left) {
+            print(2*Now+1);
+            print(2*Now+2);
+        }
+    }
+    /*
     void insert(int Num) {
         if(c<=Tree[Num].a&&Tree[Num].b<=d)
             Tree[Num].cover++;
@@ -48,7 +73,7 @@ public
         }
     }
 
-    void Count(intNum) {
+    void Count(int Num) {
         if(Tree[Num].cover)
             Number+=(Tree[Num].b-Tree[Num].a);
         else {
@@ -58,4 +83,48 @@ public
                 Count(Tree[Num].right);
         }
     }
+    */
+
+    void query(int num, int left, int right) {
+        if(Tree[num].left == left && Tree[num].right==right) {
+            maxx = max(Tree[num].max, maxx);
+            minx = min(Tree[num].min, minx);
+            return;
+        }
+        const int mid = (Tree[num].left +Tree[num].right)/2;
+        if(left > mid) {
+            query(2* num+2, left, right);
+        }
+        else if(right <= mid) {
+            query(2*num+1, left, right);
+        }
+        else {
+            query(2*num+1, left, mid);
+            query(2*num+2, mid + 1, right);
+        }
+    }
 };
+
+int main() {
+    int n, m;
+    int a, b;
+
+    Segment_Tree s;
+    cin >> n >> m;
+    for(int i = 0;i<n;i++) {
+        cin >> val[i];
+    }
+    s.build(0, 0, n-1);
+    //s.print(0);
+    for(int i=0;i<m;i++) {
+        cin >> a >> b;
+        a --;
+        b --;
+        maxx = 0;
+        minx = INT_MAX;
+        s.query(0,a,b);
+        printf("%d\n", maxx - minx);
+    }
+    return 0;
+}
+
--- shellsort.cc
+++ shellsort.cc
@@ -1,3 +1,33 @@
-//after 5-sorting
-//after 3-sorting
-//after 1-sorting
+#include<iostream>
+
+using namespace std;
+
+void shell_insert(int a[], int left, int right, int gap) {
+    int tmp;
+    int i, j;
+    for(i=left+gap;i<right;i++) {
+        tmp = a[i];
+        for(j=i-gap;tmp<a[j] && j>=left;j-=gap) {
+            a[j+gap] = a[j];
+        }
+        a[j+gap] = tmp;
+    }
+}
+
+void shell_sort(int a[], int left, int right) {
+    int gap = right - left;
+    while(gap>1) {
+        gap = gap / 3 + 1;
+        shell_insert(a, left, right, gap);
+    }
+}
+
+int main() {
+    int a[10];
+    for(int i=0;i<10;i++) cin >> a[i];
+    shell_sort(a, 0, 10);
+    for(int i=0;i<10;i++) cout<< a[i]<<" " ;
+    cout << endl;
+    return 0;
+}
+
