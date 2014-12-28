d8bef4a24a795fded34f40e32314a0bbd257b6ec
raynald <raynaldchung@gmail.com>
2014-12-28 19:15:12 +0100

    suffix array

--- suffix_array.cc
+++ suffix_array.cc
@@ -3,9 +3,7 @@
 #include <algorithm>
 using namespace std;
 
-// Structure to store information of a suffix
-struct suffix
-{
+struct suffix {
     int index; // To store original index
     int rank[2]; // To store ranks and next rank pair
 };
@@ -28,8 +26,7 @@ int *buildSuffixArray(char *txt, int n)
     // Store suffixes and their indexes in an array of structures.
     // The structure is needed to sort the suffixes alphabatically
     // and maintain their old indexes while sorting
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         suffixes[i].index = i;
         suffixes[i].rank[0] = txt[i] - 'a';
         suffixes[i].rank[1] = ((i+1) < n)? (txt[i + 1] - 'a'): -1;
@@ -45,8 +42,7 @@ int *buildSuffixArray(char *txt, int n)
     int ind[n];  // This array is needed to get the index in suffixes[]
     // from original index.  This mapping is needed to get
     // next suffix.
-    for (int k = 4; k < 2*n; k = k*2)
-    {
+    for (int k = 4; k < 2*n; k = k*2) {
         // Assigning rank and index values to first suffix
         int rank = 0;
         int prev_rank = suffixes[0].rank[0];
@@ -54,18 +50,15 @@ int *buildSuffixArray(char *txt, int n)
         ind[suffixes[0].index] = 0;
 
         // Assigning rank to suffixes
-        for (int i = 1; i < n; i++)
-        {
+        for (int i = 1; i < n; i++) {
             // If first rank and next ranks are same as that of previous
             // suffix in array, assign the same new rank to this suffix
             if (suffixes[i].rank[0] == prev_rank &&
-                    suffixes[i].rank[1] == suffixes[i-1].rank[1])
-            {
+                    suffixes[i].rank[1] == suffixes[i-1].rank[1]) {
                 prev_rank = suffixes[i].rank[0];
                 suffixes[i].rank[0] = rank;
             }
-            else // Otherwise increment rank and assign
-            {
+            else { // Otherwise increment rank and assign
                 prev_rank = suffixes[i].rank[0];
                 suffixes[i].rank[0] = ++rank;
             }
@@ -73,8 +66,7 @@ int *buildSuffixArray(char *txt, int n)
         }
 
         // Assign next rank to every suffix
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             int nextindex = suffixes[i].index + k/2;
             suffixes[i].rank[1] = (nextindex < n)?
                 suffixes[ind[nextindex]].rank[0]: -1;
@@ -94,16 +86,13 @@ int *buildSuffixArray(char *txt, int n)
 }
 
 // A utility function to print an array of given size
-void printArr(int arr[], int n)
-{
+void printArr(int arr[], int n) {
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
     cout << endl;
 }
 
-// Driver program to test above functions
-int main()
-{
+int main() {
     char txt[] = "banana";
     int n = strlen(txt);
     int *suffixArr = buildSuffixArray(txt,  n);
