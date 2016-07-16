dfa1381cefc672ee7737baa81b4259b5dcb5b85c
Raynald Chung <raynaldchung@gmail.com>
2016-07-16 14:50:46 +0200

    revised extended KMP algorihtm

--- extended_kmp.cc
+++ extended_kmp.cc
@@ -1,68 +1,74 @@
-#include<iostream>
-#include<string>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-
 class Extended_KMP {
     private:
-        static const int MAXSIZE = 400005;
-        int A[MAXSIZE], B[MAXSIZE];
+        vector<int> next; // next[i] is length of LCA of needle[i..n] and needle[0..n]
+        vector<int> match; // match[i] is lengthof LCA of haystack[i..m] and needle[0..n]
     public:
-        int max(int x, int y) {
-            return x > y ? x : y;
-        }
+        void getNext(string needle) {
+            int len = needle.length();
+            int rightmost, j, k;
 
-        void get_next(string T) {
-            int n = T.length();
-            int rightmost, L, j, k;
-            //rightmost checked
+            next.resize(len);
             j = 0;
-            while(j + 1 < n && T[j + 0] == T[j + 1]) j = j + 1;
-            A[0] = 0; 
-            A[1] = j;
+            while (j + 1 < len && needle[j] == needle[j + 1]) j ++;
+            next[0] = len; 
+            next[1] = j;
             k = 1;
-            for(int i = 2; i < n; i ++) {
-                rightmost = k + A[k] - 1;
-                L = A[i - k];
-                //T[i-k]..T[A[k]-1] == T[i]..T[rightmost]
-                if( L < rightmost - i + 1 ) A[i] = L;
-                else {
-                    j = max(0, rightmost - i + 1); //Attention
-                    while(i + j < n && T[i + j] == T[0 + j]) j = j + 1;
-                    A[i] = j;
+            // k is the index for the rightmost check
+            for (int i = 2; i < len; i ++) {
+                rightmost = k + next[k] - 1;
+                // Since needle{0 .. next[k] - 1} == needle{k .. k + next[k] - 1}
+                // Hence, it's true that needle[i - k .. next[k] - 1] == needle[i .. rightmost]
+                if ( next[i - k] < rightmost - i + 1 ) {
+                    // Just use the old result
+                    next[i] = next[i - k];
+                } else {
+                    j = max(0, rightmost - i + 1);
+                    while (i + j < len && needle[i + j] == needle[j] ) j ++;
+                    next[i] = j;
                     k = i;
                 }
             }
         }
 
-        void get_extend(string S, string T) {
-            int sn = S.length();
-            int tn = T.length();
-            int j, k, Len, L;
+        void getExtend(string haystack, string needle) {
+            int hayLength = haystack.length();
+            int neeLength = needle.length();
+            int j, k, rightmost, L;
 
-            get_next(T);
+            getNext(needle);
+            match.resize(hayLength);
             j = 0;
-            while(j < sn && j < tn && T[0 + j] == S[0 + j]) j = j + 1;
-            B[0] = j;
+            while (j < min(hayLength, neeLength) && needle[j] == haystack[j]) j ++;
+            match[0] = j;
             k = 0;
-            for(int i = 1;i < sn; i ++) {
-                Len = k + B[k] - 1;
-                L = A[i-k];
-                if( L < Len - i + 1 ) B[i] = L;
-                else {
-                    j = max(0,Len - i + 1);
-                    while(i + j < sn && j < tn && S[i + j] == T[0 + j]) j = j + 1;
-                    B[i] = j;
+            // k is the index for the rightmost check
+            for (int i = 1;i < hayLength; i ++) {
+                rightmost = k + match[k] - 1;
+                // Since needle{0 .. match[k] - 1} == haystack{{k .. k + match[k] - 1}
+                // Hence, it's true that needle[i - k .. match[k] - 1] == haystack[i .. rightmost]
+                if ( next[i - k] < rightmost - i + 1 ) {
+                    // Just use the old result
+                    match[i] = next[i - k];
+                } else {
+                    j = max(0, rightmost - i + 1);
+                    while (i + j < hayLength && j < neeLength && haystack[i + j] == needle[j]) j ++;
+                    match[i] = j;
                     k = i;
                 }
             }
+            for(auto item: next) cout << item << " "; cout << endl;
+            for(auto item: match) cout << item << " "; cout << endl;
         }
 };
 
 int main() {
     Extended_KMP kmp;
-    kmp.get_extend("abacabacabc", "abcabcab");
+    kmp.getExtend("abacabacabc", "abcabcab");
+
     return 0;
 }
 
