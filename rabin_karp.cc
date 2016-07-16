fccd50b3bfcbd042313ee0702af419ffbf94a96c
Raynald Chung <raynaldchung@gmail.com>
2016-07-16 15:15:11 +0200

    revise rabin karp algorithm

--- rabin_karp.cc
+++ rabin_karp.cc
@@ -1,49 +1,52 @@
-#include <stdio.h>
-#include <string.h>
+#include<bits/stdc++.h>
+
+using namespace std;
+
 const int R = 256; //alphabet size
 const long Q = 0xfff1; //big prime number
 
 //Hash function
-static long hash(const char key[], const int M) {
-    int j;
+long getHash(const string &key) {
     long h = 0;
-    for (j = 0; j < M; ++j) h = (h * R + key[j]) % Q;
+
+    for (int j = 0; j < key.length(); ++j) h = (h * R + key[j]) % Q;
     return h;
 }
+
 //Rehash
-static long rehash(const long h, const char first, const char next,
-        const long RM) {
+long rehash(const long h, const char first, const char next, const long RM) {
     long newh = (h + Q - RM * first % Q) % Q;
     newh = (newh * R + next) % Q;
     return newh;
 }
-//Monte Carlo method
-static int check(const char *pattern, const char substring[]) {
+
+int check(const string &pattern, const string &substring) {
+    for(int i = 0;i < pattern.length();i ++) {
+        if (pattern[i] != substring[i]) return 0;
+    }
     return 1;
 }
 
-int rabin_karp(const char *text, const char *pattern) {
-    int i;
-    const int n = strlen(text);
-    const int m = strlen(pattern);
-    const long pattern_hash = hash(pattern, m);
-    long text_hash = hash(text, m);
+int rabinKarp(const string &text, const string &pattern) {
+    const long pattern_hash = getHash(pattern);
+    long text_hash = getHash(text.substr(0, pattern.length()));
     int RM = 1;
-    for (i = 0; i < m - 1; ++i) RM = (RM * R) % Q;
-    for (i = 0; i <= n - m; ++i) {
+    for (int i = 0; i < pattern.length() - 1; ++ i) RM = (RM * R) % Q;
+    for (int i = 0; i <= text.length() - pattern.length(); ++ i) {
         if (text_hash == pattern_hash) {
             if (check(pattern, &text[i])) return i;
         }
-        text_hash = rehash(text_hash, text[i], text[i + m], RM);
+        text_hash = rehash(text_hash, text[i], text[i + pattern.length()], RM);
     }
     return -1;
 }
 
 int main() {
-    const char *text = "HERE IS A SIMPLE EXAMPLE";
-    const char *pattern = "EXAMPLE";
-    const int pos = rabin_karp(text, pattern);
-    printf("%d\n", pos); /* 17 */
+    string text = "HERE IS A SIMPLE EXAMPLE";
+    string pattern = "EXAMPLE";
+    const int pos = rabinKarp(text, pattern);
+    printf("%d\n", pos); /* expect 17 */
+
     return 0;
 }
 
